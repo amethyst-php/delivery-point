dfddd5aacf90e82a5a6b7701c5b49800bb2e185d
railken <railken@protonmail.ch>
2019-06-23 18:58:18 +0200

    fix test return type && fix composer.json

--- tests/Http/App/DeliveryPointTest.php
+++ tests/Http/App/DeliveryPointTest.php
@@ -35,7 +35,7 @@ class DeliveryPointTest extends BaseTest
     /**
      * Setup the test environment.
      */
-    public function setUp()
+    public function setUp(): void
     {
         parent::setUp();
 
--- tests/Http/App/DeliveryPointVendorTest.php
+++ tests/Http/App/DeliveryPointVendorTest.php
@@ -35,7 +35,7 @@ class DeliveryPointVendorTest extends BaseTest
     /**
      * Setup the test environment.
      */
-    public function setUp()
+    public function setUp(): void
     {
         parent::setUp();
 
