66022e8608aff489f53d6e0c394476e7d68fba58
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/DeliveryPointManager.php
+++ src/Managers/DeliveryPointManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\DeliveryPoint newEntity()
+ * @method \Amethyst\Schemas\DeliveryPointSchema getSchema()
+ * @method \Amethyst\Repositories\DeliveryPointRepository getRepository()
+ * @method \Amethyst\Serializers\DeliveryPointSerializer getSerializer()
+ * @method \Amethyst\Validators\DeliveryPointValidator getValidator()
+ * @method \Amethyst\Authorizers\DeliveryPointAuthorizer getAuthorizer()
+ */
 class DeliveryPointManager extends Manager
 {
     use ConfigurableManager;
