c8a0d31fc2b8cc54cac66164526fcb42cc130232
Railken <railken@protonmail.ch>
2020-04-01 00:21:59 +0200

    update data core

--- src/Providers/DeliveryPointServiceProvider.php
+++ src/Providers/DeliveryPointServiceProvider.php
@@ -23,7 +23,5 @@ class DeliveryPointServiceProvider extends CommonServiceProvider
     public function boot()
     {
         parent::boot();
-
-        app('amethyst')->pushMorphRelation('opening-hour', 'target', 'delivery-point');
     }
 }
